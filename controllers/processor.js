7d94a0c832b7d1a2d9dc237b145472d6085dab74
abnet <abutihere@gmail.com>
2019-03-08 17:20:07 -0800

    client acat pagination

--- controllers/processor.js
+++ controllers/processor.js
@@ -519,25 +519,26 @@ exports.fetchAllByPagination = function* fetchAllACATForms(next) {
   };
 
   try {
-    let canViewAll =  yield hasPermission(this.state._user, 'VIEW_ALL');
+    //let canViewAll =  yield hasPermission(this.state._user, 'VIEW_ALL');
     let canView =  yield hasPermission(this.state._user, 'VIEW');
     let user = this.state._user;
     let account = yield Account.findOne({ user: user._id }).exec();
 
     // Super Admin
-    if (!account || (account.multi_branches && canViewAll)) {
+    if (!account || (account.multi_branches)) {
         query = {};       
         
 
-    // Can VIEW ALL
-    } else if (canViewAll) {
-      if(account.access_branches.length) {
-          query.branch = { $in: account.access_branches.slice() };
+    //TODO: But need to be considered....
+        // Can VIEW ALL
+    // } else if (canViewAll) {
+    //   if(account.access_branches.length) {
+    //       query.branch = { $in: account.access_branches.slice() };
 
-      } else if(account.default_branch) {
-          query.branch = account.default_branch;
+    //   } else if(account.default_branch) {
+    //       query.branch = account.default_branch;
 
-      }  
+    //   }  
       
     // Can VIEW
     } else if(canView) {
